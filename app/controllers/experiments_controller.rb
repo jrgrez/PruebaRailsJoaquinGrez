41ab39e4754f23d8c7592bf207e89eeb4ed38ec3
Joaquin Grez <joaquingrez@gmail.com>
2017-06-21 20:57:30 -0300

    Partial ready

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,10 +1,13 @@
 class ExperimentsController < ApplicationController
   def pag1
+  	render layout: 'bootstrap'
   end
 
   def pag2
+  	render layout: 'bootstrap'
   end
 
   def pag3
+  	render layout: 'bootstrap'
   end
 end
